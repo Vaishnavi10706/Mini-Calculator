4b3d29cd0b33c9adadbc2adebe2cfae7aa48f058
Vaishnavi10706 <vaishnavii10706@gmail.com>
2025-08-19 09:56:39 +0530

    added statement

--- simplecalculator.py
+++ simplecalculator.py
@@ -4,12 +4,12 @@ num2 = float(input("Enter 2nd number: "))
 print("press 1 for addition \npress 2 for substraction \npress 3 for multiplication \npress 4 for division")
 choice = int(input("select operation: "))
 if choice == 1:
-  print(num1+num2)
+  print("the addition of two numbers is",num1+num2)
 elif choice == 2:
-  print(num1-num2)
+  print("the substraction of two numbers is",num1-num2)
 elif choice == 3:
-  print(num1*num2)
+  print("the multiplication of two numbers is",num1*num2)
 elif choice == 4:
-  print(num1/num2)
+  print("the division of two numbers is",num1/num2)
 else :
   print("invalid operation selected")
